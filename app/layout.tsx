d81e5f6a1c17139bd81e1dbb531454845d2a44ef
Piotr Dębski <piotrdb1@gmail.com>
2023-09-05 22:45:35 +0200

    add robots, sitemap and metadata for seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Wynajem Kobierzyce',
-  description: 'Wynajem mieszkania niedaleko Bielan Wrocławskich',
-}
+  title: "Wynajem - Kobierzyce",
+  description:
+    "Wynajem mieszkania, lokali, noclegu w miejscowości Wiezrzbice, gmina Kobierzyce - niedaleko Bielany Wrocławskie, Kąty Wrocławskie i Wrocław. Krótkoterminowo i długoterminowo",
+  colorScheme: "dark",
+  keywords: ["Kobierzyce", "Wrocławskie", "Wynajem", "Nocleg"],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pl">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
