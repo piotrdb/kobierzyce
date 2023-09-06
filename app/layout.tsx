8d0ffe05bc9a8ea73180174200b1034f8d66d48f
Piotr Dębski <piotrdb1@gmail.com>
2023-09-06 22:27:36 +0200

    add twitter card and opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,28 @@ export const metadata: Metadata = {
   description:
     "Wynajem mieszkania, noclegu, lokalu w miejscowości Wiezrzbice, gmina Kobierzyce - niedaleko Bielany Wrocławskie, Kąty Wrocławskie i Wrocław.",
   colorScheme: "dark",
-  keywords: ["Kobierzyce", "Wrocławskie", "Wynajem", "Nocleg"],
+  keywords: ["Kobierzyce", "Wrocławskie", "Wynajem", "Nocleg", "Kwatery"],
+  openGraph: {
+    title: "Wynajem - Kobierzyce | Kwatery pracownicze, lokale",
+    description:
+      "Wynajem mieszkania, noclegu, lokalu w miejscowości Wiezrzbice, gmina Kobierzyce - niedaleko Bielany Wrocławskie, Kąty Wrocławskie i Wrocław.",
+    type: "article",
+    url: "https://www.nocleg-kobierzyce.pl",
+    images: [
+      {
+        url: "https://www.nocleg-kobierzyce.pl/api/og?title=Generate Wynajem - Kobierzyce | Kwatery pracownicze, lokale",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Wynajem - Kobierzyce | Kwatery pracownicze, lokale",
+    description:
+      "Wynajem mieszkania, noclegu, lokalu w miejscowości Wiezrzbice, gmina Kobierzyce - niedaleko Bielany Wrocławskie, Kąty Wrocławskie i Wrocław.",
+    images: [
+      "https://www.nocleg-kobierzyce.pl/api/og?title=Generate Wynajem - Kobierzyce | Kwatery pracownicze, lokale",
+    ],
+  },
 };
 
 export default function RootLayout({
